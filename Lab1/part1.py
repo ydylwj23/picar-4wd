129b5de2238d545b594b5e979e985a8c921432c9
Jue Wang <ydylydyl@gmail.com>
2021-02-06 15:06:56 -0800

    better random

--- Lab1/part1.py
+++ Lab1/part1.py
@@ -3,6 +3,9 @@ import random
 
 speed = 30
 
+# 0: forward 1: left 2: right
+dir = 0
+
 def main():
     while True:
         scan_list = fc.scan_step(35)
@@ -11,12 +14,23 @@ def main():
 
         tmp = scan_list[3:7]
         print(tmp)
+
+        global dir
+
         if tmp != [2,2,2,2]:
-            if random.random() < 0.5:
-                fc.turn_right(speed)
-            else:
+            if dir == 0:
+                if random.random() < 0.5:
+                    fc.turn_right(speed)
+                    dir = 2
+                else:
+                    fc.turn_left(speed)
+                    dir = 1
+            else if dir == 1:
                 fc.turn_left(speed)
+            else :
+                fc.turn_right(speed)
         else:
+            dir = 0
             fc.forward(speed)
 
 if __name__ == "__main__":
