4297cef49e920624f6925447eb6fcf7b5943f247
Jue Wang <ydylydyl@gmail.com>
2021-02-06 14:34:29 -0800

    create part1.py

---
+++ Lab1/part1.py
@@ -0,0 +1,26 @@
+import picar_4wd as fc
+import random
+
+speed = 30
+
+def main():
+    while True:
+        scan_list = fc.scan_step(35)
+        if not scan_list:
+            continue
+
+        tmp = scan_list[3:7]
+        print(tmp)
+        if tmp != [2,2,2,2]:
+            if random.random() < 0.5:
+                fc.turn_right(speed)
+            else:
+                fac.turn_left(speed)
+        else:
+            fc.forward(speed)
+
+if __name__ == "__main__":
+    try: 
+        main()
+    finally: 
+        fc.stop()
